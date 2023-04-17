471c1f9d1b51d9f9637351b1b3d2641b77630730
Rashmi Bhagya <89590611+RashmiBhagya@users.noreply.github.com>
2023-04-17 20:56:34 +0530

    add updates

---
+++ apps/products/src/interface/product.interface.ts
@@ -0,0 +1,8 @@
+import { Document } from 'mongoose';
+export interface IProduct extends Document{
+    readonly productName: string;
+    readonly catergory: string;
+    readonly productId: string;
+    readonly Discription: string;
+    readonly quantity: number;
+}
